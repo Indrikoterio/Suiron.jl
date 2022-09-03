208adc49aa97c36499036112bb453a68ca691a34
Cleve <indriko@yahoo.com>
2022-09-03 21:03:43 +0900

    Formatting.

--- src/Add.jl
+++ src/Add.jl
@@ -75,6 +75,26 @@ function sr_add(arguments::Vector{Unifiable},
 end # sr_add
 
 #===============================================================
+  unify - unifies the result of a function with another term,
+  usually a variable.
+
+  Params: Add predicate
+          other unifiable term
+          substitution set
+  Returns:
+          updated substitution set
+          success/failure flag
+===============================================================#
+function unify(a::Add, other::Unifiable,
+               ss::SubstitutionSet)::Tuple{SubstitutionSet, Bool}
+    result, ok = sr_add(a.terms, ss)
+    if !ok
+        return ss, false
+    end
+    return unify(result, other, ss)
+end
+
+#===============================================================
  recreate_variables - The scope of a logic variable is the rule
  in which it is defined. Please refer to LogicVar.jl.
 
@@ -85,24 +105,4 @@ end # sr_add
 function recreate_variables(a::Add, vars::DictLogicVars)::Expression
     new_terms = recreate_vars(a.terms, vars)
     return Add(new_terms)
-end
-
-#===============================================================
-  unify - unifies the result of a function with another term,
-  usually a variable.
-
-  Params:
-     other unifiable term
-     substitution set
-  Returns:
-     updated substitution set
-     success/failure flag
-===============================================================#
-function unify(a::Add, other::Unifiable,
-               ss::SubstitutionSet)::Tuple{SubstitutionSet, Bool}
-    result, ok = sr_add(a.terms, ss)
-    if !ok
-        return ss, false
-    end
-    return unify(result, other, ss)
 end
